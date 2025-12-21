c50a2eaeee4664622f15ddc972366f1ecf07ee90
uchmod777 <kyle.fluto@gmail.com>
2025-12-21 13:34:59 -0600

    Modified cors and added toml file for deployment

---
+++ app/config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+    ],
+
+    'allowed_origins_patterns' => [
+        '/https:\/\/.*\.vercel\.app$/',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
